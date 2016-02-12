c92e653890bac03ab3d94adb2ec1440b6d17d968
jodiberdis <berdis@nmsu.edu>
2016-02-12 08:56:17 -0700

    Delete q11.py

--- q11/q11.py
+++
@@ -1,5 +0,0 @@
-#If you are looking at faint, background-limited, stars, how much fainter can you see (i.e., measure at the same S/N) if you have 0.3 arcsec instead of 1.0 arcsec images? Express your answer in magnitudes. 
-
-import numpy as np
-
-SN = S / (np.sqrt())
